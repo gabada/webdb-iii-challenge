b2d4b7dde3f2505ec210da9ed76d4206831130dc
Gill Abada <gill@callifi.com>
2019-03-27 15:36:53 -0400

    setup structure for node.

---
+++ index.js
@@ -0,0 +1,24 @@
+const server = require('./api/server.js');
+const cors = require('cors');
+const helmet = require('helmet');
+
+const knexConfig = {
+  client: 'sqlite3',
+  connection: {
+    filename: './data/lambda.sqlite3'
+  },
+  useNullAsDefault: true
+};
+
+const db = knex(knexConfig);
+const server = express();
+const port = 5000;
+
+const cohortsRoutes = require('./cohorts/cohorts-router.js');
+
+server.use(express.json());
+server.use(cors());
+server.use(helmet());
+server.use('/api/cohorts', cohortsRoutes);
+
+server.listen(port, () => console.log(`\n** Running on port ${port} **\n`));
