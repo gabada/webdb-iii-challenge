adbb3c547fb1a4c1bcf37e9071936497a54fca10
Gill Abada <gill@callifi.com>
2019-03-27 17:09:53 -0400

    add student API endpoints.

--- index.js
+++ index.js
@@ -6,10 +6,12 @@ const server = express();
 const port = 5000;
 
 const cohortsRoutes = require('./cohorts/cohorts-router.js');
+const studentsRoutes = require('./students/students-router.js');
 
 server.use(express.json());
 server.use(cors());
 server.use(helmet());
 server.use('/api/cohorts', cohortsRoutes);
+server.use('/api/students', studentsRoutes);
 
 server.listen(port, () => console.log(`\n** Running on port ${port} **\n`));
---
+++ students/students-router.js
@@ -0,0 +1,79 @@
+const router = require('express').Router();
+const knex = require('knex');
+
+const knexConfig = {
+  client: 'sqlite3',
+  connection: {
+    filename: './data/lambda.sqlite3'
+  },
+  useNullAsDefault: true
+};
+const db = knex(knexConfig);
+
+router.get('/', async (req, res) => {
+  try {
+    const students = await db('students');
+    res.status(200).json(students);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+router.get('/:id', async (req, res) => {
+  try {
+    const student = await db('students')
+      .where({ id: req.params.id })
+      .first();
+    res.status(200).json(student);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+router.post('/', async (req, res) => {
+  try {
+    const [id] = await db('students').insert(req.body);
+
+    const student = await db('students')
+      .where({ id })
+      .first();
+
+    res.status(201).json(student);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+router.put('/:id', async (req, res) => {
+  try {
+    const count = await db('students')
+      .where({ id: req.params.id })
+      .update(req.body);
+
+    if (count > 0) {
+      const student = await db('students')
+        .where({ id: req.params.id })
+        .first();
+
+      res.status(200).json(student);
+    } else {
+      res.status(404).json({ message: 'Records not found' });
+    }
+  } catch (error) {}
+});
+
+router.delete('/:id', async (req, res) => {
+  try {
+    const count = await db('students')
+      .where({ id: req.params.id })
+      .del();
+
+    if (count > 0) {
+      res.status(204).end();
+    } else {
+      res.status(404).json({ message: 'Records not found' });
+    }
+  } catch (error) {}
+});
+
+module.exports = router;
